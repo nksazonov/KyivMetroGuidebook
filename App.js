8950bd55df48738c5f2ffd49fed9db0bf77b54ab
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-19 18:52:51 +0200

    [ ADDED ] safe area, RN-svg, MapScreen mockup

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
+import MapScreen from './src/screens/MapScreen'
+
+const App = MapScreen;
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaProvider>
+      <App />
+    </SafeAreaProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/MapScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const MapScreen = ()=> {
+  return (
+    <SafeAreaView>
+      <Text>I'm the Map</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default MapScreen;
