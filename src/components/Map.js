079b7b159d88b8e1c534f062f1eb61ba6eccf8b0
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-03-10 20:31:43 +0200

    [ MOVED ] Map component from MapScreen

---
+++ src/components/Map.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, Dimensions } from 'react-native'
+import Svg, { G } from 'react-native-svg';
+import ZoomableSvg from 'ns-zoomable-svg';
+
+import map from '../svg/map'
+
+const { width, height } = Dimensions.get('window');
+
+class SvgRoot extends Component {
+  render() {
+    const { transform } = this.props;
+    return (
+      <Svg width={width} height={height}>
+        <G transform={transform}>
+          { map }
+        </G>
+      </Svg>
+    );
+  }
+}
+
+export default class Map extends Component {
+  state = {
+    type: 1,
+    constrain: true,
+    constraints: {
+      combine: 'dynamic',
+			scaleExtent: [1, 4],
+      translateExtent: [
+				[0, 700],			//[minX - fx, minY - fy]
+				[1333, 700],	//[maxX + fx, maxY + fy]
+			],
+    },
+  };
+
+  render() {
+    const { constrain, constraints } = this.state;
+    return (
+        <View style={styles.container}>
+          <ZoomableSvg
+            align="mid"
+            vbWidth={1333}
+            vbHeight={1333}
+            width={width}
+            height={height}
+            doubleTapThreshold={300}
+            doubleTapZoom={4}
+            meetOrSlice="meet"
+            svgRoot={SvgRoot}
+            childProps={this.childProps}
+            constrain={constrain ? constraints : null}
+          />
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ecf0f1',
+	},
+});
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,74 +1,14 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Dimensions, Animated } from 'react-native';
-import Svg, { G, Circle, Path, Rect } from 'react-native-svg';
-
-import Map from '../svg/map'
-
-import ZoomableSvg from 'ns-zoomable-svg';
-const { width, height } = Dimensions.get('window');
-
-class SvgRoot extends Component {
-  render() {
-    const { transform } = this.props;
-    return (
-      <Svg width={width} height={height}>
-        <G transform={transform}>
-					
-					{Map}
-
-        </G>
-      </Svg>
-    );
-  }
-}
-
-const constraintCombinations = [
-  'none',
-  'dynamic',
-  'static',
-  'union',
-  'intersect',
-];
+import { View, StyleSheet } from 'react-native';
+import Map from '../components/Map'
 
 export default class MapScreen extends Component {
-  state = {
-    type: 1,
-    constrain: true,
-    constraints: {
-      combine: 'dynamic',
-			scaleExtent: [1, 4],
-      translateExtent: [
-				[0, 700],			//[minX - fx, minY - fy]
-				[1333, 700],	//[maxX + fx, maxY + fy]
-			],
-    },
-  };
 
   render() {
-    const { constrain, constraints } = this.state;
     return (
-      <View style={styles.container}>
-        <ZoomableSvg
-          align="mid"
-          vbWidth={1333}
-          vbHeight={1333}
-          width={width}
-          height={height}
-					doubleTapThreshold={300}
-					doubleTapZoom={4}
-          meetOrSlice="meet"
-          svgRoot={SvgRoot}
-          childProps={this.childProps}
-          constrain={constrain ? constraints : null}
-        />
-
-      </View>
+        <View>
+          <Map />
+        </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ecf0f1',
-	},
-});
