4f4307f5ebef47182ae5fc11ceeb48f789765880
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-03-24 23:05:11 +0200

    [ ADDED ] arrival, active, setRouteStation

--- src/context/RouteContext.js
+++ src/context/RouteContext.js
@@ -1,23 +1,51 @@
 import createDataContext from './createDataContext';
 
-const initialState = { departure: '', arrival: '', route: [] };
+const initialState = { departure: '', arrival: '', route: [], active: '' };
 
 const routeReducer = (state, action) => {
   switch(action.type) {
     case 'set_departure':
       return { ...state, departure: action.payload };
 
+    case 'set_arrival':
+      return { ...state, arrival: action.payload };
+
+    case 'set_active':
+      return { ...state, active: action.payload };
+
     default:
       return state;
   }
+}
+
+const setActive = dispatch => (stationId) => {
+  dispatch({ type: 'set_active', payload: stationId });
 }
 
 const setDeparture = dispatch => (stationId) => {
   dispatch({ type: 'set_departure', payload: stationId });
 }
 
+const setArrival = dispatch => (stationId) => {
+  dispatch({ type: 'set_arrival', payload: stationId });
+}
+
+const setRouteStation = () => (stationId, departure, arrival) => {
+  if (!departure) {
+    setDeparture(stationId);
+    return;
+  } else {
+    if (!arrival) {
+      setArrival(stationId);
+      return;
+    } else {
+      setActive(stationId);
+    }
+  }
+}
+
 export const { Provider, Context } = createDataContext(
   routeReducer,
-  { setDeparture },
+  { setDeparture, setArrival, setRouteStation, setActive },
   initialState
 )
