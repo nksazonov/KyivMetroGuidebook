24c653bdf681920d21d48e540a6d39e0f99df327
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-03-27 23:31:27 +0200

    [ ADDED ] blue, mediumgrey

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,8 @@
 export default {
   lightgrey: "#f0f0f0",
+  mediumgrey: '#e0e0e0',
   grey: "#bfbfbf",
   darkblue: "#323232",
-  blackblue: "#080808"
+  blackblue: "#080808",
+  blue: "#367af7"
 }
