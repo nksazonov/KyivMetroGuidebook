76d6430203a442f39ba5cab52b3f4e390af94923
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-03-27 23:37:23 +0200

    [ DONE ] station search

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -3,23 +3,26 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 import ZoomableMap from '../components/ZoomableMap'
 import { vw, vh } from 'react-native-css-vh-vw'
 import DestStationsBar from '../components/DestStationsBar'
+import SearchStationBlock from '../components/SearchStationBlock'
 
 const MapScreen = () => {
     return (
         <View style={styles.container}>
           <ZoomableMap />
           <DestStationsBar style={ styles.destStationsBar } />
+          <SearchStationBlock style={ styles.searchStationBlock } />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    position: 'relative'
   },
   destStationsBar: {
     width: vw(100),
-    height: vw(15),
+    height: vw(13.5),
     position: 'absolute',
     bottom: 0,
     backgroundColor: '#fcfcfc',
@@ -27,6 +30,13 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
 
+    elevation: 24,
+  },
+  searchStationBlock: {
+    position: 'absolute',
+    bottom: 0,
+    width: vw(100),
+    height: vh(90),
     elevation: 24,
   }
 })
