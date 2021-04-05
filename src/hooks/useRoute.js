aaaf4e86c3732a0c5d23f7ffb62a89946cdff095
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-04-05 16:45:57 +0300

    [ DONE ] route duration to console

--- src/hooks/useRoute.js
+++ src/hooks/useRoute.js
@@ -1,6 +1,17 @@
+import Graph from 'graph-data-structure'
+import pathsData from '../data/paths'
+
+const paths = Graph();
+
+for (const path of pathsData) {
+  const { from, to, time } = path;
+  paths.addEdge(from, to, time);
+  paths.addEdge(to, from, time);
+}
+
 export function buildRoute(departure, arrival) {
   if (departure && arrival && departure !== arrival) {
-    console.log(departure, '->', arrival);
+    console.log(paths.shortestPath(departure.toString(), arrival.toString()).weight);
   }
 
   return [];
