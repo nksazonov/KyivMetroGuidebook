860006c1ffaa36f7ccb1610f45c62294f5b04555
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-04-05 21:07:24 +0300

    [ ADDED ] fonts

--- src/styles/dimensions.js
+++ src/styles/dimensions.js
@@ -1,6 +1,11 @@
 export default {
   iconContainerWidth: 20,
   paddingSide: 15,
+  smallPaddingSide: 10,
   iconMargin: 10,
   iconHighlightable: 30,
+  hugeFont: 32,
+  bigFont: 25,
+  mediumFont: 17,
+  smallFont: 12,
 }
